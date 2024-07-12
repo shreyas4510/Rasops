993a29880a56e53d3e4bbf3ba4002d33bb720283
Shreyas Deshmukh <44319899+shreyas4510@users.noreply.github.com>
2024-07-12 11:07:58 +0530

    fix: email path resolved (#84)

--- order-management-be/src/api/services/email.service.js
+++ order-management-be/src/api/services/email.service.js
@@ -1,18 +1,25 @@
 import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import Mustache from 'mustache';
 import { transporter } from '../../config/email.js';
 import env from '../../config/env.js';
 import logger from '../../config/logger.js';
 import { EMAIL_ACTIONS, CustomError } from '../utils/common.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const getEmailData = (action, payload) => {
-    let path = '';
+    let filePath = path.resolve(`${__dirname}`).split('src')[0];
     let template = '';
     let url = '';
+
+    logger('info', 'email filepath', filePath);
     switch (action) {
         case EMAIL_ACTIONS.VERIFY_USER:
-            path = `${process.cwd()}/src/api/templates/verifyEmail.html`;
-            template = readFileSync(path, 'utf8');
+            filePath += `/src/api/templates/verifyEmail.html`;
+            template = readFileSync(filePath, 'utf8');
             url = `${env.app.appUrl}/verify?token=${encodeURIComponent(payload.token)}`;
 
             return {
@@ -20,8 +27,8 @@ const getEmailData = (action, payload) => {
                 template: Mustache.render(template, { appUrl: url })
             };
         case EMAIL_ACTIONS.FORGOT_PASSWORD:
-            path = `${process.cwd()}/src/api/templates/forgotPassword.html`;
-            template = readFileSync(path, 'utf8');
+            filePath += `/src/api/templates/forgotPassword.html`;
+            template = readFileSync(filePath, 'utf8');
             url = `${env.app.appUrl}/reset?token=${encodeURIComponent(payload.token)}`;
 
             return {
@@ -29,8 +36,8 @@ const getEmailData = (action, payload) => {
                 template: Mustache.render(template, { appUrl: url })
             };
         case EMAIL_ACTIONS.INVITE_MANAGER:
-            path = `${process.cwd()}/src/api/templates/inviteManager.html`;
-            template = readFileSync(path, 'utf8');
+            filePath += `/src/api/templates/inviteManager.html`;
+            template = readFileSync(filePath, 'utf8');
             url = `${env.app.appUrl}/signup?token=${encodeURIComponent(payload.token)}`;
 
             return {
@@ -41,16 +48,16 @@ const getEmailData = (action, payload) => {
                 })
             };
         case EMAIL_ACTIONS.CUSTOM_SUBSCRIPTION:
-            path = `${process.cwd()}/src/api/templates/customSubscription.html`;
-            template = readFileSync(path, 'utf8');
+            filePath += `/src/api/templates/customSubscription.html`;
+            template = readFileSync(filePath, 'utf8');
 
             return {
                 subject: 'Re: Custom Subscription Request',
                 template: Mustache.render(template, { ...payload })
             };
         case EMAIL_ACTIONS.INVOICE_EMAIL:
-            path = `${process.cwd()}/src/api/templates/invoiceEmail.html`;
-            template = readFileSync(path, 'utf8');
+            filePath += `/src/api/templates/invoiceEmail.html`;
+            template = readFileSync(filePath, 'utf8');
 
             return {
                 subject: 'Re: Customer Invoice',
